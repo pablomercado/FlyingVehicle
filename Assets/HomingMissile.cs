9be36bd829200e1e56025d3f8ec538d23c69bc24
Pablo <pablomercadomx@gmail.com>
2017-02-11 13:18:03 +0700

    Update proyect

---
+++ Assets/HomingMissile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomingMissile : MonoBehaviour {
+
+    [SerializeField] private float speed = 10f;
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectsWithTag("MissileTarget")[0].transform;
+
+    }
+
+	void Update () {
+        //Target.transform.position
+        Vector3 targetDirection = target.transform.position - transform.position;
+        Debug.Log(targetDirection.magnitude);
+        Vector3 movingVector = (transform.forward + targetDirection) * Time.deltaTime * speed;
+
+        transform.position += movingVector;
+        transform.LookAt(targetDirection);
+	}
+}
