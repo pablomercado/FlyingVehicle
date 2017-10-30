cd3ff635232078def09714b8f4689dea29ca1e0e
Pablo <pablomercadomx@gmail.com>
2017-10-30 21:14:57 +0700

    Added bezier group mover

---
+++ Assets/Scripts/MoveGroupBezier.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MoveGroupBezier : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] bezierPointsGO;
+    private BezierEvaluator bezierEvaluator;
+
+    private void Start()
+    {
+        setupCurve();
+    }
+
+    private void setupCurve()
+    {
+//        bezierEvaluator = new BezierEvaluator();
+//        var bezierPoints = new Vector3[bezierPointsGO.Length];
+//        for (int i = 0; i < bezierPoints.Length; i++)
+//            bezierPoints[i] = bezierPointsGO[i].transform.position;
+//        bezierEvaluator.SetupCurve(bezierPoints);        
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+
+        if (bezierEvaluator != null)
+        {
+            Gizmos.DrawLine(bezierEvaluator.Evaluate(0f), bezierEvaluator.Evaluate(1f));
+            var curvePresicion = 30f;
+            if (bezierPointsGO.Length > 1)
+                for (var t = 0; t < curvePresicion; t++)
+                    Gizmos.DrawLine(bezierEvaluator.Evaluate(t / curvePresicion), bezierEvaluator.Evaluate((t + 1)/ curvePresicion));    
+        }
+        
+    }
+    
+}
